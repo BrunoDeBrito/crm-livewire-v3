5ad4ef29a51b703cb26818d6332fbb73e68ca627
Bruno DeBrito <brunordebrito@gmail.com>
2026-07-22 16:53:30 -0300

    CRM-14: Confirming before deletion

--- app/Livewire/Admin/Users/Delete.php
+++ app/Livewire/Admin/Users/Delete.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Admin\Users;
 
 use App\Models\User;
 use Illuminate\View\View;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 /**
@@ -17,6 +18,11 @@ class Delete extends Component
 {
     public User $user;
 
+    #[Rule(['required', 'string', 'confirmed'])]
+    public string $confirmation = "DART VADER";
+
+    public ?string $confirmation_confirmation = null;
+
     public function render(): View
     {
         return view('livewire.admin.users.delete');
@@ -24,6 +30,7 @@ class Delete extends Component
 
     public function destroy(): void
     {
+        $this->validate();
         $this->user->delete();
 
         $this->dispatch('user::deleted');
