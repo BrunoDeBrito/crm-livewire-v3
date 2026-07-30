73f9ebbee09f33e7f04dc9f3a264631548f670bf
Bruno DeBrito <brunordebrito@gmail.com>
2026-07-29 23:19:51 -0300

    CRM-13: list detail user

--- resources/views/livewire/admin/users/show.blade.php
+++ resources/views/livewire/admin/users/show.blade.php
@@ -1,10 +1,26 @@
 <div>
-    @if($user)
-        {{ $user->name }}
-        {{ $user->email }}
-        {{ $user->created_at->format('d/m/Y H:i') }}
-        {{ $user->updated_at->format('d/m/Y H:i') }}
-        {{ $user->deleted_at->format('d/m/Y H:i') }}
-        {{ $user->deletedBy->name }}
-    @endif
+
+    <x-modal wire:model="modal" :title="$user?->name" separator="">
+        @if($user)
+            <x-input readonly label="Name" :value="$user->name"/>
+            <x-input readonly label="Email" :value="$user->email"/>
+            <x-input readonly label="Created At" :value="$user->created_at->format('d/m/Y H:i')"/>
+            <x-input readonly label="Updated At" :value="$user->updated_at->format('d/m/Y H:i')"/>
+            <x-input readonly label="Deleted At" :value="$user->deleted_at?->format('d/m/Y H:i')"/>
+            <x-input readonly label="Deleted By" :value="$user->deletedBy?->name"/>
+        @endif
+
+        <x-slot:actions>
+
+            <x-button
+                @click="$wire.modal = false"
+                label="Close"
+                color="secondary"
+                squared
+            />
+
+        </x-slot:actions>
+
+    </x-modal>
+
 </div>
